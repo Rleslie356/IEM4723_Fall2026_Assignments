1c22b662574692a7ed73fbab31fd25e6416add19
Rleslie356 <ryan.leslie11@okstate.edu>
2026-09-13 19:02:01 -0500

    Complete Question 2

--- docker_commands.sh
+++ docker_commands.sh
@@ -4,3 +4,5 @@ docker tag linear-regression-app linear-regression-app:v0.1.1
 docker tag linear-regression-app linear-regression-app:v0.1.2
 docker tag linear-regression-app rleslie356/linear-regression-app:latest
 docker push rleslie356/linear-regression-app:latest
+
+# Question 2 
